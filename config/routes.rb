48f147a940535b88b2a803f543c609864da8dc35
alejandro <alejandroweil13@gmail.com>
2021-03-16 16:21:42 -0400

    Finished user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    root to: 'root#root'
+    namespace :api, defaults: {format: :json} do
+      resource :session, only: %i[create destroy]
+      resources :users, only: %i[create]
+    end
 end
