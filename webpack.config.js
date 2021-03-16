bf446a96afd8b1b7aa186f52d2c23344ddea0227
alejandro <alejandroweil13@gmail.com>
2021-03-16 11:44:53 -0400

    Finished up git ignore and added //= jquery to application.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './frontend/entry.jsx',
+  entry: './frontend/chartmycourse.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
