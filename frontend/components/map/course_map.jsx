f3de548c4e7809252f2d8cbbe348fbdeda7bf649
alejandro <alejandroweil13@gmail.com>
2021-03-24 10:41:48 -0400

    Working on map

--- frontend/components/map/course_map.jsx
+++ frontend/components/map/course_map.jsx
@@ -3,28 +3,26 @@ import {withRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 
 
-const mapOptions = {
-    center: {
-        lat: 40.785091,
-        lng: -73.968285
-    },
-    zoom: 14
-};
 
 class courseMap extends React.Component{
     componentDidMount(){
+        const mapOptions = {
+            center: {
+                lat: 40.785091,
+                lng: -73.968285
+            },
+            zoom: 14
+        };
         this.map = new google.maps.Map(this.mapNode, mapOptions);
-    }
-
-
+    };
 
 render(){
     return(
-        <div ref={map => this.mapNode = map} id='map'>
+        <div ref={map => (this.mapNode = map)} id='map'>
             map
         </div>
     )
   }
 }
 
-export default withRouter(courseMap);
+export default courseMap;
