4af93f2fe15ca83bfba2894155b526a873bf31ee
alejandro <alejandroweil13@gmail.com>
2021-03-25 15:20:04 -0400

    Working on creating markers in map

--- frontend/components/map/course_map.jsx
+++ frontend/components/map/course_map.jsx
@@ -14,7 +14,28 @@ class courseMap extends React.Component{
             zoom: 14
         };
         this.map = new google.maps.Map(this.mapNode, mapOptions);
+        const marker = new google.maps.Marker({
+            position: {
+                lat: 40.785091,
+                lng: -73.968285
+            },
+            map: this.map
+        })
+        this.placeMarker = this.placeMarker.bind(this);
+        this.map.addListener("click", (e) => {
+            placeMarker(e.latLng, map)
+        });
     };
+    
+
+
+    placeMarker(latLng, map){
+        new google.maps.Marker({
+            position: latLng,
+            map: this.map
+        });
+    }
+
 
 render(){
     return(
