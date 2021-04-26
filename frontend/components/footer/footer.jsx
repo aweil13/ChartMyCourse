43c485a76564c7cbf5024814a4d1fb735fa42aab
alejandro <alejandroweil13@gmail.com>
2021-04-26 13:14:11 -0400

    Changes to footer, added links for personal sites

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -11,14 +11,17 @@ const Footer = () => {
                     <li className='social-title-li'>
                       <span className='social-title'>ABOUT ME</span>
                     </li>
-                    <li className='social-list-element'><Link to='/'>Linked In</Link></li>
-                    <li className='social-list-element'><Link to='/'>Check out my Github</Link></li>
+                    <li className='social-list-element'><a href='https://www.linkedin.com/in/alejandro-weil-b9275720b/'>Linked In</a></li>
+                    <li className='social-list-element'><a href='https://github.com/aweil13'>Check out my Github</a></li>
+                    <li className='social-list-element'><a href='https://angel.co/u/alejandro-weil'>AngelList</a></li>
+                    <li className='social-list-element'><a href='https://aweil13.github.io/PortfolioSite/'>Personal Site</a></li>    
                 </ul>
                 <ul className='help-ul'>
                   <li className='help-title-li'>
                       <span className='help-title'>HELP</span>
                   </li>
-                  <li className='help-list-element'><Link to='/login'>Log In / Register</Link></li>
+                  <li className='help-list-element'><Link to='/login'>Log In</Link></li>
+                  <li className='help-list-element'><Link to='/signup'>Sign Up</Link></li>
                   <li className='help-list-element'><a href="https://github.com/aweil13/ChartMyCourse/wiki">Website Repo</a></li>
                 </ul>
             </div>
