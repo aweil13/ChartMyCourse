f21424f10f6a2ae110b0c60c401c450d81d84025
alejandro <alejandroweil13@gmail.com>
2021-05-05 23:05:30 -0400

    Dashboard component being fleshed out

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -12,15 +12,26 @@ class DashboardComponent extends React.Component {
 
     render(){
         let array = [];
+        const user = this.props.currentUser
         Object.values(this.props.courses).map(course => {array.push(course)})
-        console.log(array);
-        console.log(this.props.courses)
-        console.log(this.props.currentUser)
         return (
-            <div className='dashboard-container'>
-                <div></div>
-                <Footer/>
-            </div>
+            <>
+              <div className='dashboard-container'>
+                <div className='courses-container'>
+                    <div className='user-welcome-container'>
+                        <h1 className='user-welcome-message'>
+                            Welcome {user.first_name} {user.last_name}
+                        </h1>
+                    </div>
+                    <div className='courses-title-container'>
+                        <h2 className='courses-title'>
+                            Courses Created:
+                        </h2>
+                    </div>
+                </div>
+              </div>
+              <Footer/>
+            </>
         )
     }
 }
