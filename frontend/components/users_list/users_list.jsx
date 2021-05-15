895692b9ba802db0052c5e06f6ac0a722f6ad56a
alejandro <alejandroweil13@gmail.com>
2021-05-15 00:20:28 -0400

    Made users list

--- frontend/components/users_list/users_list.jsx
+++ frontend/components/users_list/users_list.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {fetchAllUsers} from '../../actions/users_actions';
 import Footer from '../footer/footer';
 
@@ -31,11 +32,13 @@ class UsersList extends React.Component {
                 <h1 className='users-title'>USERS</h1>
                 <div className='users-container'>
                     {Object.values(users).map(user => (
-                        <div className='user-container' key={user.id}>
+                        <div className='user-container' key={user.id} >
                             <span> {user.username} </span>
                             <span> {user.first_name} {user.last_name}</span>
                            { this.props.currentUser.id === user.id ?  <button className='add-friend-button'>It's Me!</button> :
                              <button className='add-friend-button'>Add Friend</button> }
+                           { this.props.currentUser.id === user.id ?  <Link to={'/dashboard'} className='view-courses-button'><span>Dashboard</span></Link> :
+                             <Link to={`/users/${user.id}`} className='view-courses-button'><span>View Courses</span></Link> }
                         </div>
                     ))}
                 </div>
